9f6603101a6dc5ee5856515eb7d2f8cf92a82bb5
Ewa Jochymková <ewa.jochymkova@seznam.cz>
2024-10-31 20:52:58 +0100

    add array of objects with projects

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,20 +1,20 @@
 import photo from "../images/me.jpg"
 
 const AboutMe = () => {
-  return <section className="h-screen flex flex-col justify-around items-center mt-52 md:flex-row">
-    <div className="flex flex-col justify-center items-center w-[20rem] h-[40rem] md:w-[30rem] md:h-[40rem] ">
+  return <section className="h-screen flex flex-col justify-evenly items-center mt-16 md:flex-row">
+    <div className="flex flex-col justify-center items-center w-[20rem] h-[40rem] md:w-[25rem] md:h-[40rem] lg:w-[35rem]">
       <h2 className="font-bold text-xl pb-5 ">O mně</h2>
       <p className="text-wrap text-[#cbcfcd] text-center" >
       Po dokončení studia účetnictví a daní jsem nastoupila jako účetní v e-shopu. Už tehdy jsem věděla, že to není obor, u kterého bych chtěla zůstat celý život. Po narození dcery jsem přemýšlela, jakým jiným směrem bych se mohla vydat. Naštěstí mi bratr doporučil, abych zkusila programování,i když jsem na začátku nevěřila, že zrovna já bych se v oboru IT dokázala uplatnit. </p>
       <br />
-      <p className="text-wrap text-[#cbcfcd] text-center"> Začala jsem se učit Python a rychle jsem zjistila, že mě programování hrozně baví. Po ukončení kurzu jsem ale došla k závěru, že se chci zaměřit na frontend. Proto jsem se pustila do studia HTML, CSS a poté i JavaScriptu. </p>
+      <p className="text-wrap text-[#cbcfcd] text-center"> Začala jsem se učit Python a rychle jsem zjistila, že mě programování hrozně baví. Po dokončení kurzu jsem ale došla k závěru, že se chci zaměřit na frontend. Proto jsem se pustila do studia HTML, CSS a poté i JavaScriptu. </p>
       <br />
-      <p className="text-wrap text-[#cbcfcd] text-center"> Jelikož už od narození dcera špatně spí, nebylo času na učení mnoho. Díky podpoře manžela a babiček jsem si však dokázala najít prostor na studium a dál se v programování zlepšovat.</p>
+      <p className="text-wrap text-[#cbcfcd] text-center"> Protože dcera od narození špatně spí, nebylo času na učení mnoho. Díky podpoře manžela a babiček jsem si však dokázala najít prostor na studium a dál se v programování zlepšovat.</p>
       <br />
-      <p className="text-wrap text-[#cbcfcd] text-center"> Nikdy dřív mě učení nebavilo tak jako teď. Programování mě naplňuje, protože v sobě propojuje logické myšlení s kreativitou.
+      <p className="text-wrap text-[#cbcfcd] text-center"> Nikdy dřív mě učení nebavilo tak jako teď. Programování mě naplňuje, protože v sobě propojuje logické myšlení s kreativitou. Zároveň to, že člověk vytvoří něco z ničeho, ve mě vyvolává obrovskou radost.
       </p>
     </div>
-    <img src={photo} alt="personal-photo" className="hidden h-[20rem] rounded-xl md:h-[25rem] md:block" />
+    <img src={photo} alt="personal-photo" className="hidden md:h-[25rem] rounded-xl  md:block lg:mt-12 " />
   </section>
 }
 
--- src/components/OneProject.tsx
+++ src/components/OneProject.tsx
@@ -3,11 +3,35 @@ import flashcards from "../images/flashcards.png"
 import recipes from "../images/recipes.png"
 
 const OneProject = () => {
+  const projects = [{
+    id: 1,
+    image: {pregnancydiet},
+    name: "Pregnancy diet",
+    about: "Jedná se o webovou aplikaci pro těhotné ženy, ve které si mohou vyhledat, zda jsou jednotlivé potraviny vhodné ke konzumaci během těhotenství. Aplikace je vytvořená v Reactu a data jsou načítána z databáze Firebase Firestore.",
+    githublink: "https://github.com/EwaJ94/pregnancy-diet",
+    weblink: "https://pregnancy-diet.netlify.app/"
+  },
+  {
+    id: 2,
+    image: {flashcards},
+    name: "Flashcards",
+    about: "Webová aplikace pro snadné učení například slovíček. Aplikace je vytvořena v JavaScriptu.",
+    githublink: "https://github.com/EwaJ94/flashcards",
+    weblink: "https://flashcards0.netlify.app/"
+  },
+  {
+    id: 3,
+    image: {recipes},
+    name: "My recipes",
+    about: "Aplikace umožňuje vyhledávání receptů podle klíčového slova nebo kategorie. Uživatel si může oblíbené recepty uložit. Aplikace je vytvořená v Reactu a data jsou získávána prostřednictvím API.",
+    githublink: "https://github.com/EwaJ94/my-recipes",
+    weblink: "https://myrecipes0.netlify.app/"
+  }
+]
+
   return <section className="">
     <div className="">
-        {/* <img src={pregnancydiet} alt="project pregnancy diet" className=""/>
-        <img src={flashcards} alt="project flashcards" className=""/>
-        <img src={recipes} alt="project recipes" className=""/> */}
+    
     </div>
   </section>
 }
